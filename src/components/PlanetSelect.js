347fe11051b5cfee40532f92a0d5813ae5bd12da
Anton Bukhman <diamondspb2015@gmail.com>
2023-03-11 14:00:47 +0200

    ready v2

--- src/components/PlanetSelect.js
+++ src/components/PlanetSelect.js
@@ -3,28 +3,32 @@ import React, { Component } from "react";
 import saveToLocalStorage from "../utils/saveToLocalStorage";
 import getPlanetsFromBack from "../utils/getPlanetsFromBack";
 import loadFromLocalStorage from "../utils/loadFromLocalStorage";
-import { logDOM } from "@testing-library/react";
 
 class PlanetSelect extends Component {
+  constructor(props) {
+    super(props);
+    this.updatePlanets();
+  }
+
   async updatePlanets() {
     const oldPlanets = JSON.parse(loadFromLocalStorage("planets"));
     if (
       !oldPlanets ||
       Date.now() - Date(oldPlanets["date"]) > 30 * 24 * 60 * 60 * 1000
     ) {
-      saveToLocalStorage("planets", await getPlanetsFromBack());
+      await saveToLocalStorage("planets", await getPlanetsFromBack());
     }
   }
-  async componentDidMount() {
-    await this.updatePlanets();
-  }
+
   getPlanetList() {
-    const obj = JSON.parse(loadFromLocalStorage("planets"));
-    const planetList = [];
-    Object.entries(structuredClone(obj["data"])).forEach((elem) => {
-      planetList.push(elem[1]["name"]);
-    }, []);
-    return structuredClone(planetList);
+    if (loadFromLocalStorage("planets")) {
+      const obj = JSON.parse(loadFromLocalStorage("planets"));
+      const planetList = [];
+      Object.entries(structuredClone(obj["data"])).forEach((elem) => {
+        planetList.push(elem[1]["name"]);
+      }, []);
+      return structuredClone(planetList);
+    }
   }
 
   render() {
@@ -32,17 +36,19 @@ class PlanetSelect extends Component {
       <div>
         <label htmlFor="planet">Planet</label>
         <select id="planet" name="country">
-          <option value="australia" disabled>
+          <option value="#" disabled>
             Choose your planet
           </option>
 
-          {this.getPlanetList().map((elem) => {
-            return (
-              <option key={elem} value={elem}>
-                {elem}
-              </option>
-            );
-          })}
+          {this.getPlanetList()
+            ? this.getPlanetList().map((elem) => {
+                return (
+                  <option key={elem} value={elem}>
+                    {elem}
+                  </option>
+                );
+              })
+            : []}
         </select>
       </div>
     );
